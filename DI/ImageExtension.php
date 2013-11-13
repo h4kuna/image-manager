75b6351fdad9563905b26f28fbbbdc6443207804
Milan Matejcek <milan.matejcek@gmail.com>
2013-11-13 10:49:46 +0100

    Class alias if is need

--- DI/ImageExtension.php
+++ DI/ImageExtension.php
@@ -6,6 +6,16 @@ use Nette\Configurator;
 use Nette\DI\CompilerExtension;
 use Nette\DI\Compiler;
 
+if (defined('\Nette\Framework::VERSION_ID') || Framework::VERSION_ID < 20100) {
+    if (!class_exists('Nette\DI\CompilerExtension')) {
+        class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+    }
+
+    if (!class_exists('Nette\DI\Compiler')) {
+        class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+    }
+}
+
 class ImageExtension extends CompilerExtension {
 
     public $defaults = array(
